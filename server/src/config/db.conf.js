517c53b8186c2d08eba0628e243c4b4d3c7b7956
phongho01 <phong.ho@napaglobal.com>
2023-06-01 15:41:16 +0700

    change config

--- server/src/config/db.conf.js
+++ server/src/config/db.conf.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const DB_CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.neq96gg.mongodb.net/faucet`
+const DB_CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.neq96gg.mongodb.net/${process.env.DB_NAME}`
 
 const connectDB = () => {
   return mongoose.connect(DB_CONNECTION_URL);

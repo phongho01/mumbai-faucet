de1f84ee893b4934e6e36488e04fb97271b8322b
phongho01 <phong.ho@napaglobal.com>
2023-06-06 16:18:05 +0700

    add config redis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const { createClient } = require('redis');
 const { start, login } = require('./utils/discord');
 
-const client = createClient();
+// const client = createClient();
+
+const client = createClient({
+  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+});
 
 start().then(async () => {
   client.on('error', (err) => console.log('Redis Client Error', err));
